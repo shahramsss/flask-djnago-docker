f82afe0b481437ef2c4cf81a33e98e96f6669a96
shahramsss <shahramsss1987@gmail.com>
2025-11-04 11:37:34 +0330

    add django dockerize

--- django/volumes/flash_card/flash_card/settings.py
+++ django/volumes/flash_card/flash_card/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,13 +86,21 @@ WSGI_APPLICATION = "flash_card.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("MARIADB_DATABASE"),
+        "USER": os.environ.get("MARIADB_USER"),
+        "PASSWORD": os.environ.get("MARIADB_PASSWORD"), 
+        "HOST": os.environ.get("MARIADB_HOST"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
